a2cc194847eeb1b5733d609baf441faf20a17b7d
hanieh98 <h.akhavan1998@gmail.com>
2021-11-15 12:08:10 +0330

    modify

--- bama.py
+++ bama.py
@@ -3,62 +3,72 @@ import requests as rq
 from csv import writer
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--nop', type=int, help='number of pages', default=850)
-parser.add_argument('--path', type=str, default='/home/hanieh/car/pages/',
-                    help='path of a directory to save html pages')
-parser.add_argument('--output_path', type=str, default='/home/hanieh/car/carslist.csv',
-                    help='path of the directory to save output')
+def main():
+    pages = []
+    cars = []
+    models = []
+    brands = []
+    years = []
+    page_name = []
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/50.0.2661.102 Safari/537.36'}
 
-args = parser.parse_args()
+    # get ads' links of first n  page of bama
+    for n in range(args.nop, 830, -1):
+        url = 'https://bama.ir/car/all-brands/all-models/all-trims?pic=true&page=' + str(n)
+        print(url)
+        r = rq.get(url, headers=headers)
+        soup = BeautifulSoup(r.text, "html.parser")
+        links = soup.select('div.title a')
+        for l in links:
+            pages.append(l['href'])
 
-pages = []
-cars = []
-models = []
-brands = []
-years = []
-page_name = []
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    # get info from each ad
+    for p in pages:
+        print(p)
+        name = p.split('-')
+        r2 = rq.get(p, headers=headers)
+        soup2 = BeautifulSoup(r2.text, "html.parser")
+        with open(args.path + str(name[1]) + '.html', 'w') as f:
+            f.write(r2.text)
+        tag = soup2.select('h1.addetail-title span')
+        images = soup2.select("div.hidden-xs a img")
+        if len(tag[0].find_all()) != 0:
+            brand = tag[1].text
+            model = tag[2].text
+            year = tag[3].text
+        else:
+            brand = tag[0].text
+            model = tag[1].text
+            year = tag[2].text
+        for img in images:
+            if img['src'] not in cars:
+                cars.append(img['src'])
+                brands.append(brand)
+                models.append(model)
+                years.append(year)
+                page_name.append(name[1])
 
-#get ads' links of first n  page of bama
-for n in range (args.nop,830,-1):
-    url = 'https://bama.ir/car/all-brands/all-models/all-trims?pic=true&page='+str(n)
-    print(url)
-    r = rq.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, "html.parser")
-    links = soup.select('div.title a')
-    for l in links:
-        pages.append(l['href'])
+    # write images' link, name, brand of an ad to csv file
+    with open(args.output_path, 'a') as f:
+        wr = writer(f)
+        for i in range(len(cars)):
+            wr.writerow([page_name[i], cars[i], brands[i], models[i], years[i]])
 
-#get info from each ad
-for p in pages:
-    print(p)
-    name = p.split('-')
-    r2 = rq.get(p, headers=headers)
-    soup2 = BeautifulSoup(r2.text, "html.parser")
-    with open(args.path+str(name[1])+'.html', 'w') as f:
-        f.write(r2.text)
-    tag = soup2.select('h1.addetail-title span')
-    images= soup2.select("div.hidden-xs a img")
-    if len(tag[0].find_all()) != 0:
-        brand = tag[1].text
-        model = tag[2].text
-        year = tag[3].text
-    else:
-        brand = tag[0].text
-        model = tag[1].text
-        year = tag[2].text
-    for img in images:
-        if img['src'] not in cars:
-            cars.append(img['src'])
-            brands.append(brand)
-            models.append(model)
-            years.append(year)
-            page_name.append(name[1])
 
-#write images' link, name, brand of an ad to csv file
-with open(args.output_path, 'a') as f:
-    writer = writer(f)
-    for i in range(len(cars)):
-        writer.writerow([page_name[i], cars[i], brands[i], models[i], years[i]])
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--nop', type=int, help='number of pages', default=850)
+    parser.add_argument('--path', type=str, default='/home/hanieh/car/pages/',
+                        help='path of a directory to save html pages')
+    parser.add_argument('--output_path', type=str, default='/home/hanieh/car/carslist.csv',
+                        help='path of the directory to save output')
+    arguments = parser.parse_args()
+    return arguments
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    main()
--- bama_crawler.py
+++ bama_crawler.py
@@ -2,41 +2,49 @@ import requests as rq
 from csv import writer
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--nop', type=int, help='number of pages', default=775)
-parser.add_argument('--output_path', type=str, default='/home/hanieh/car/carslist.csv',
-                    help='path of the directory to save output')
 
-args = parser.parse_args()
+def main():
+    cars_image = []
+    models = []
+    brands = []
+    years = []
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/50.0.2661.102 Safari/537.36'}
+    # get ads' links of first n  page of bama
+    for n in range(args.nop, 700, -1):
+        url = 'https://bama.ir/car/search?pageIndex=' + str(n) + '&image=1'
+        print(url)
+        r = rq.get(url, headers=headers)
+        cars = r.json()['data']['car_ads']
+        for car in cars:
+            title = car['detail']['title'].split('،')
+            model = title[1]
+            brand = title[0]
+            images = car['detail']['images']
+            year = car['detail']['year']
+            for i in images:
+                cars_image.append(i['large'])
+                years.append(year)
+                brands.append(brand)
+                models.append(model)
+    # #write images, name, brand of an ad to csv file
+    with open(args.output_path, 'a') as f:
+        wr = writer(f)
+        for i in range(len(cars_image)):
+            wr.writerow([cars_image[i], brands[i], models[i], years[i]])
 
-cars_image = []
-models = []
-brands = []
-years = []
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) '
-                  'Chrome/50.0.2661.102 Safari/537.36'}
 
-# get ads' links of first n  page of bama
-for n in range(args.nop, 770, -1):
-    url = 'https://bama.ir/car/search?pageIndex=' + str(n) + '&image=1'
-    print(url)
-    r = rq.get(url, headers=headers)
-    cars = r.json()['data']['car_ads']
-    for car in cars:
-        title = car['detail']['title'].split('،')
-        model = title[1]
-        brand = title[0]
-        images = car['detail']['images']
-        year = car['detail']['year']
-        for i in images:
-            cars_image.append(i['large'])
-            years.append(year)
-            brands.append(brand)
-            models.append(model)
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--nop', type=int, help='number of pages', default=775)
+    parser.add_argument('--output_path', type=str, default='/home/hanieh/car/carslist.csv',
+                        help='path of the directory to save output')
 
-# #write images, name, brand of an ad to csv file
-with open(args.output_path, 'a') as f:
-    writer = writer(f)
-    for i in range(len(cars_image)):
-        writer.writerow([cars_image[i], brands[i], models[i], years[i]])
+    arguments = parser.parse_args()
+    return arguments
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    main()
